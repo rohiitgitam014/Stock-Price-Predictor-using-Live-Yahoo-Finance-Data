80cf94243ef66ef3778866de634c83aee8f6e9ad
rohiitgitam014 <145531927+rohiitgitam014@users.noreply.github.com>
2025-07-04 15:42:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import streamlit as st
 from model import train_and_predict
-
+import pandas as pd
 st.set_page_config(page_title="📈 Stock Price Predictor")
 
 st.title("📊 Real-Time Stock Price Predictor")
