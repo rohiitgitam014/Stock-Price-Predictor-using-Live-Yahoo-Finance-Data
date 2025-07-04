a9a450f0d96f9432d899070251f83465c2021348
rohiitgitam014 <145531927+rohiitgitam014@users.noreply.github.com>
2025-07-04 13:04:50 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,10 @@
 import yfinance as yf
 import pandas as pd
 
-def fetch_stock_data(ticker="AAPL", period="10d", interval="1h"):
+def fetch_stock_data(ticker="RELIANCE.NS", period="30d", interval="1h"):
     df = yf.download(ticker, period=period, interval=interval)
     df.dropna(inplace=True)
     df["Next_Close"] = df["Close"].shift(-1)
     df.dropna(inplace=True)
     return df
+
